83a1b9f1e3575e24949cc0e93a14cf3541ad4873
ttolaaa <somtolaaa@gmail.com>
2023-01-07 12:40:12 +0900

    code comments

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
+// import dotenv
 import * as dotenv from 'dotenv';
+
+// access env variable define in .env file
 dotenv.config();
 
+// initialize openai sdk
 import { Configuration, OpenAIApi } from 'openai';
 
+// create config object from Configuration with require api key
 const configuration = new Configuration({
   apiKey: process.env.OPENAI,
 });
 
+//initialize openai sdk
 const openai = new OpenAIApi(configuration);
 
 import express from 'express';
 import cors from 'cors';
 
+// create app object express()
 const app = express();
-app.use(cors());
-app.use(express.json());
+
+// add middleware to application & enable CORS for all routes
+app.use(cors());  // middleware adds CORS headers to responses
+app.use(express.json()); //  middleware parses the JSON body of incoming requests.
+
 
 app.post('/dream', async (req, res) => {
     const prompt = req.body.prompt;
